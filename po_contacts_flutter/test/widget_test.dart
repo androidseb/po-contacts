4f6a128ad990d0f9c378a415c4a6a2dc1061d936
androidseb <theandroidseb@gmail.com>
2020-01-02 16:32:53 -0500

    moved the root 'MyApp' widget to a separate file as 'RootView'

--- po_contacts_flutter/test/widget_test.dart
+++ po_contacts_flutter/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:po_contacts_flutter/main.dart';
+import 'package:po_contacts_flutter/view/root_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(RootView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
