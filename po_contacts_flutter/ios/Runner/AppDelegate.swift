6d132945e4b0ff98141eca3fa6b7e7b45326e3dd
androidseb <theandroidseb@gmail.com>
2020-01-18 21:04:52 -0500

    implemented the native functions for iOS

--- po_contacts_flutter/ios/Runner/AppDelegate.swift
+++ po_contacts_flutter/ios/Runner/AppDelegate.swift
@@ -13,15 +13,8 @@ import Flutter
                                                binaryMessenger: controller.binaryMessenger)
     filesApiChannel.setMethodCallHandler({
       (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      if call.method == "getInboxFileId" {
-      } else if call.method == "getInboxFileId" {
-        result(nil)
-      } else if call.method == "discardInboxFileId" {
-        result(nil)
-      } else if call.method == "getCopiedInboxFilePath" {
-        result(nil)
-      } else if call.method == "shareFileExternally" {
-        result(nil)
+      if call.method == "shareFileExternally" {
+        self.shareFileExternally(call: call, result: result)
       } else {
         result(FlutterMethodNotImplemented)
       }
@@ -29,5 +22,28 @@ import Flutter
     //END OF CUSTOM CHANNEL CODE HERE
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func shareFileExternally(call: FlutterMethodCall, result: FlutterResult) {
+    guard let callArgs = call.arguments else {
+      return
+    }
+    var filePath = ""
+    if let callArgsCast = callArgs as? [String: Any] {
+      filePath = callArgsCast["filePath"] as? String ?? ""
+    } else {
+      filePath = ""
+    }
+    if filePath == "" {
+      result(nil)
+      return
+    }
+    let fileURL = NSURL(fileURLWithPath: filePath)
+    var fileURLs = [Any]()
+    fileURLs.append(fileURL)
+    let activityViewController = UIActivityViewController(activityItems: fileURLs, applicationActivities: nil)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    controller.present(activityViewController, animated: true, completion: nil)
+    result(nil)
   }
 }
--- po_contacts_flutter/lib/controller/native_apis_controller.dart
+++ po_contacts_flutter/lib/controller/native_apis_controller.dart
@@ -7,15 +7,61 @@ import 'package:path_provider/path_provider.dart';
 class NativeApisController {
   static const platform = const MethodChannel('com.exlyo.pocontacts/files');
 
+  Future<String> _getInboxFileIdInDirForIOS(final Directory dir, final String inboxDirName) async {
+    final List<FileSystemEntity> filesList = dir.listSync();
+    FileSystemEntity foundInboxDir;
+    for (final FileSystemEntity fse in filesList) {
+      if (!fse.path.endsWith('/$inboxDirName')) {
+        continue;
+      }
+      final fileStat = await fse.stat();
+      if (fileStat.type != FileSystemEntityType.directory) {
+        continue;
+      }
+      foundInboxDir = fse;
+      break;
+    }
+    if (foundInboxDir == null) {
+      return null;
+    }
+    final List<FileSystemEntity> inboxFilesList = Directory(foundInboxDir.path).listSync();
+    for (final FileSystemEntity fse in inboxFilesList) {
+      return fse.path;
+    }
+    return null;
+  }
+
+  Future<String> _getInboxFileIdForIOS() async {
+    String foundInboxFileId;
+    final Directory applicationDocumentsDir = await getApplicationDocumentsDirectory();
+    foundInboxFileId = await _getInboxFileIdInDirForIOS(applicationDocumentsDir, 'Inbox');
+    if (foundInboxFileId != null) {
+      return foundInboxFileId;
+    }
+    final Directory applicationTmpDir = await getTemporaryDirectory();
+    foundInboxFileId = await _getInboxFileIdInDirForIOS(applicationTmpDir, 'com.exlyo.pocontacts-Inbox');
+    return foundInboxFileId;
+  }
+
   Future<String> getInboxFileId() async {
+    if (Platform.isIOS) {
+      return _getInboxFileIdForIOS();
+    }
     return await platform.invokeMethod('getInboxFileId');
   }
 
   Future<void> discardInboxFileId(final String inboxFileId) async {
+    if (Platform.isIOS) {
+      await File(inboxFileId).delete();
+      return;
+    }
     return await platform.invokeMethod('discardInboxFileId', {'inboxFileId': inboxFileId});
   }
 
   Future<String> getCopiedInboxFilePath(final String inboxFileId) async {
+    if (Platform.isIOS) {
+      return inboxFileId;
+    }
     return await platform.invokeMethod('getCopiedInboxFilePath', {'inboxFileId': inboxFileId});
   }
 
