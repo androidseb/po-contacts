06dbf3246411e887843f842e2119313119676aee
androidseb <theandroidseb@gmail.com>
2020-01-22 08:04:08 -0500

    wrote some basic test for contact export

--- po_contacts_flutter/test/unit/vcf_serializer_test.dart
+++ po_contacts_flutter/test/unit/vcf_serializer_test.dart
@@ -11,6 +11,37 @@ class MockVCFWriter extends VCFWriter {
   }
 }
 
+final Contact testContact1 = Contact(
+  0, //id
+  '', //image
+  'First name', //firstName
+  'Last name', //lastName
+  'Nickname', //nickName
+  'Full Name', //fullName
+  [], //phoneInfos
+  [], //emailInfos
+  [], //addressInfos
+  'Organization Name', //organizationName
+  'Organization Division', //organizationDivision
+  'Organization Title', //organizationTitle
+  'www.website.com', //website
+  'Notes\nAnd\nmore\nlines', //notes
+  [], //unknownVCFFieldLines
+);
+
+const String CONTACT1_EXPECTED_OUTPUT = '''BEGIN:VCARD\r
+VERSION:2.1\r
+FN:Full Name\r
+N:First name;Last name;;;\r
+NICKNAME:Nickname\r
+ORG:Organization Name;Organization Division;\r
+TITLE:Organization Title\r
+URL:www.website.com\r
+NOTE:Notes\\nAnd\\nmore\\nlines\r
+END:VCARD\r
+\r
+''';
+
 void main() {
   test('VCF export - empty', () {
     final List<Contact> contacts = [];
@@ -18,5 +49,14 @@ void main() {
     final MockVCFWriter vcfWriter = MockVCFWriter();
     VCFSerializer.writeToVCF(contacts, vcfWriter, progressCallback);
     expect(vcfWriter.writtenLines.length, 0);
+  });
+
+  test('VCF export - 1 contact', () async {
+    final List<Contact> contacts = [];
+    contacts.add(testContact1);
+    final Function(int progress) progressCallback = (final int progress) {};
+    final MockVCFWriter vcfWriter = MockVCFWriter();
+    await VCFSerializer.writeToVCF(contacts, vcfWriter, progressCallback);
+    expect(vcfWriter.writtenLines.join(), CONTACT1_EXPECTED_OUTPUT);
   });
 }
