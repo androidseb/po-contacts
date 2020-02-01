f12eeb5b7910258c0713cd80fc8f9816b87d360d
androidseb <theandroidseb@gmail.com>
2020-01-31 23:17:56 -0500

    fixed prod deploy script by adding phone number from env vars

--- scripts/ios_prod_deploy.sh
+++ scripts/ios_prod_deploy.sh
@@ -3,5 +3,6 @@ set -e
 
 cd $(git rev-parse --show-toplevel)
 
+echo $APPLE_REVIEW_PHONE_NUMBER>resources/AppleStore/metadata/review_information/phone_number.txt
 sh scripts/subscripts/deploy_ios_app_update.sh prod
 
