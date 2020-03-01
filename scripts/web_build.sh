7e94904a3d26bf47520078bdb87d9a248a6e37c6
androidseb <theandroidseb@gmail.com>
2020-03-01 15:55:42 -0500

    updated web build script to use the tmp dir

--- scripts/web_build.sh
+++ scripts/web_build.sh
@@ -3,11 +3,22 @@ set -e
 
 cd $(git rev-parse --show-toplevel)
 
-echo "Editing source file locally for the web..."
-sed -i 's/import.*.mobile\.dart.*//g' po_contacts_flutter/lib/controller/platform/platform_specific_controller.dart
-sed -i 's/\ \ \ \ if\ (dart\.library\.io)/import/g' po_contacts_flutter/lib/controller/platform/platform_specific_controller.dart
-echo "Executing flutter build web..."
-cd po_contacts_flutter
-flutter build web
-echo "Build successfull!"
+echo "Copying 'po_contacts_flutter' to 'bin/tmp'..."
+rm -rf bin/tmp
+rm -rf bin/web
+cp -r po_contacts_flutter bin/tmp
 
+cd bin/tmp
+
+echo "Editing source file locally for the web..."
+sed -i 's/import.*.mobile\.dart.*//g' lib/controller/platform/platform_specific_controller.dart
+sed -i 's/\ \ \ \ if\ (dart\.library\.io)/import/g' lib/controller/platform/platform_specific_controller.dart
+echo "Executing flutter build web..."
+flutter pub get
+flutter build web
+
+cd $(git rev-parse --show-toplevel)
+mv bin/tmp/build/web bin/web
+rm -rf bin/tmp
+
+echo "Web app successfully built under 'bin/web'"
--- scripts/web_local_deploy.sh
+++ scripts/web_local_deploy.sh
@@ -6,7 +6,6 @@ cd $(git rev-parse --show-toplevel)
 echo "Building the web app into build/web..."
 sh scripts/web_build.sh
 echo "Web app built!"
-cd po_contacts_flutter/build/web
+cd bin/web
 echo "Starting local HTTP server on port 8000: http://localhost:8000"
 python -m SimpleHTTPServer 8000
-
