deebe8f444c81107f03baa9864d673ee4c25fbe1
androidseb <theandroidseb@gmail.com>
2020-03-05 06:34:16 -0500

    fixed the version for with a temporary hack

--- po_contacts_flutter/lib/model/version_info.dart
+++ po_contacts_flutter/lib/model/version_info.dart
@@ -1,7 +1,9 @@
 import 'package:package_info/package_info.dart';
 
 class VersionInfo {
-  String _appVersion = '...';
+  //TODO add back the '...' string once the package_info package is implemented on web
+  //See progress here: https://github.com/flutter/flutter/projects/69#card-30399023
+  String _appVersion = '1.1.0+23';
 
   VersionInfo() {
     PackageInfo.fromPlatform().then((final PackageInfo packageInfo) {
