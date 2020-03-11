f5db5c59b17c11034b742bf53b2e6708d1c42e44
androidseb <theandroidseb@gmail.com>
2020-03-11 07:49:41 -0400

    implemented a script to build the linux app as a zip

--- scripts/subscripts/build_linux_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd $(git rev-parse --show-toplevel)
-
-cd scripts/subscripts
-
-NWJS_FILE_NAME="nwjs-v0.43.6-linux-x64.tar.gz"
-wget https://dl.nwjs.io/v0.43.6/nwjs-v0.43.6-linux-x64.tar.gz --continue
