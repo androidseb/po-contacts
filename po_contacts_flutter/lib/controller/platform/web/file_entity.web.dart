b5d21bf768a47c349217597b10eede27ef7d989f
androidseb <theandroidseb@gmail.com>
2020-03-11 06:24:34 -0400

    fixed web file entity readaslines function not getting rid of \r chars

--- po_contacts_flutter/lib/controller/platform/web/file_entity.web.dart
+++ po_contacts_flutter/lib/controller/platform/web/file_entity.web.dart
@@ -78,7 +78,16 @@ class FileEntityWeb extends FileEntity {
       return List<String>();
     }
     final String fileContentAsString = utf8.decode(fileData);
-    return fileContentAsString.split('\n');
+    final List<String> intermediateResult = fileContentAsString.split('\n');
+    final List<String> result = [];
+    for (final String s in intermediateResult) {
+      if (s.endsWith('\r')) {
+        result.add(s.substring(0, s.length - 1));
+      } else {
+        result.add(s);
+      }
+    }
+    return result;
   }
 
   @override
