5a4a360a5b3deac9ef5fdc5707631ff6cf08193f
androidseb <theandroidseb@gmail.com>
2020-05-24 18:53:44 -0400

    introduced the syncCancelationHandler

--- po_contacts_flutter/lib/utils/cloud_sync/sync_prodedure.dart
+++ po_contacts_flutter/lib/utils/cloud_sync/sync_prodedure.dart
@@ -1,6 +1,7 @@
 import 'package:po_contacts_flutter/controller/platform/common/file_entity.dart';
 import 'package:po_contacts_flutter/utils/cloud_sync/data/remote_file.dart';
 import 'package:po_contacts_flutter/utils/cloud_sync/sync_controller.dart';
+import 'package:po_contacts_flutter/utils/cloud_sync/sync_exception.dart';
 import 'package:po_contacts_flutter/utils/cloud_sync/sync_interface.dart';
 
 class SyncInitialData<T> {
@@ -17,31 +18,56 @@ class SyncInitialData<T> {
   );
 }
 
-class SyncProcedure<T> {
-  final SyncController syncController;
-  final SyncInterface syncInterface;
+class SyncCancelationHandler {
+  final SyncProcedure syncProcedure;
   bool _canceled = false;
-
-  SyncProcedure(this.syncController, this.syncInterface);
 
   void cancel() {
     _canceled = false;
   }
 
+  SyncCancelationHandler(this.syncProcedure);
+
+  void checkForCancelation() {
+    if (_canceled) {
+      throw SyncException(SyncExceptionType.CANCELED);
+    }
+  }
+}
+
+class SyncProcedure<T> {
+  final SyncController _syncController;
+  final SyncInterface _syncInterface;
+  SyncCancelationHandler _cancelationHandler;
+
+  SyncProcedure(this._syncController, this._syncInterface) {
+    _cancelationHandler = SyncCancelationHandler(this);
+  }
+
+  void cancel() {
+    _cancelationHandler.cancel();
+  }
+
   Future<SyncInitialData> _initializeSync() async {
-    final List<T> localItems = await syncController.getLocalItems();
-    final FileEntity lastSyncedFile = syncInterface.getLastSyncedFile();
-    final List<T> lastSyncedItems = await syncController.fileEntityToItemsList(
+    final List<T> localItems = await _syncController.getLocalItems();
+    _cancelationHandler.checkForCancelation();
+    final FileEntity lastSyncedFile = _syncInterface.getLastSyncedFile();
+    _cancelationHandler.checkForCancelation();
+    final List<T> lastSyncedItems = await _syncController.fileEntityToItemsList(
       lastSyncedFile,
-      syncInterface.encryptionKey,
+      _syncInterface.encryptionKey,
     );
-    final FileEntity latestCloudFile = await syncInterface.getLatestCloudFile();
-    final List<T> remoteItems = await syncController.fileEntityToItemsList(
+    _cancelationHandler.checkForCancelation();
+    final FileEntity latestCloudFile = await _syncInterface.getLatestCloudFile();
+    _cancelationHandler.checkForCancelation();
+    final List<T> remoteItems = await _syncController.fileEntityToItemsList(
       latestCloudFile,
-      syncInterface.encryptionKey,
+      _syncInterface.encryptionKey,
     );
-    final RemoteFile currentIndexFile = syncInterface.selectedCloudIndexFile;
-    final String fileETag = await syncInterface.getFileETag(currentIndexFile.fileId);
+    _cancelationHandler.checkForCancelation();
+    final RemoteFile currentIndexFile = _syncInterface.selectedCloudIndexFile;
+    _cancelationHandler.checkForCancelation();
+    final String fileETag = await _syncInterface.getFileETag(currentIndexFile.fileId);
     return SyncInitialData(
       localItems,
       lastSyncedItems,
@@ -59,8 +85,11 @@ class SyncProcedure<T> {
   }
 
   Future<void> execute() async {
+    _cancelationHandler.checkForCancelation();
     final SyncInitialData syncInitialData = await _initializeSync();
+    _cancelationHandler.checkForCancelation();
     final List<T> syncResult = await _computeSyncResult(syncInitialData);
+    _cancelationHandler.checkForCancelation();
     await _finalizeSync(syncResult, syncInitialData.remoteFileETag);
   }
 }
