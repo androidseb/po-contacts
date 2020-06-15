43ed0e4787040dec61ef362a401e0688198158a8
androidseb <theandroidseb@gmail.com>
2020-06-14 20:10:43 -0400

    fixed the deploy script to work with python 3

--- scripts/web_local_deploy.sh
+++ scripts/web_local_deploy.sh
@@ -8,4 +8,4 @@ sh scripts/web_build.sh
 echo "Web app built!"
 cd bin/web
 echo "Starting local HTTP server on port 8000: http://localhost:8000"
-python -m SimpleHTTPServer 8000
+python -m http.server
